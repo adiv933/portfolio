8b731aafe0d15f209a8d0c0173f0b025bf877a97
Aditya Verma <adityav090304@gmail.com>
2025-07-06 12:02:29 +0530

    Add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/next"
 import { PreloaderProvider } from './contexts/PreloaderContext.jsx'
 import Preloader from './components/Preloader.jsx'
 import App from './App.jsx'
@@ -8,6 +9,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <PreloaderProvider>
       <BrowserRouter>
         <Preloader />
