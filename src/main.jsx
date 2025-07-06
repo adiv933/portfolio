7a21dcaa6406968f407ef010d9ebd8262ca608d8
Aditya Verma <adityav090304@gmail.com>
2025-07-06 16:38:39 +0530

    Add plausible analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Analytics } from "@vercel/analytics/next"
 import { PreloaderProvider } from './contexts/PreloaderContext.jsx'
 import Preloader from './components/Preloader.jsx'
 import App from './App.jsx'
@@ -9,7 +8,6 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Analytics />
     <PreloaderProvider>
       <BrowserRouter>
         <Preloader />
